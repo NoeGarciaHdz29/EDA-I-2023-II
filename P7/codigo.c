328cb1b9247c31759b750cb04cbceab970a4ea6f
Noe García Hernández <noegarciahernandez128@gmail.com>
2023-05-09 18:40:10 +0000

    Cambios P7 y P8

--- P7/codigo.c
+++ P7/codigo.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Definición de la estructura de un nodo de la lista
 struct Node {
     int data;
     struct Node* next;
 };
 
-// Función para imprimir la lista ligada circular
 void printList(struct Node* head) {
     struct Node* current = head;
 
-    // Si la lista está vacía, imprimir mensaje
     if (head == NULL) {
         printf("La lista está vacía.\n");
         return;
     }
 
-    // Si la lista no está vacía, imprimir cada elemento
     do {
         printf("%d --> ", current->data);
         current = current->next;
@@ -26,17 +22,14 @@ void printList(struct Node* head) {
     printf("\n");
 }
 
-// Función para buscar un elemento en la lista ligada circular
 void search(struct Node* head, int value) {
     struct Node* current = head;
 
-    // Si la lista está vacía, imprimir mensaje
     if (head == NULL) {
         printf("La lista está vacía.\n");
         return;
     }
 
-    // Si la lista no está vacía, buscar el elemento
     do {
         if (current->data == value) {
             printf("%d se encuentra en la estructura!\n", value);
@@ -53,12 +46,10 @@ int main() {
     struct Node* second = NULL;
     struct Node* third = NULL;
 
-    // Asignar memoria para cada nodo
     head = (struct Node*) malloc(sizeof(struct Node));
     second = (struct Node*) malloc(sizeof(struct Node));
     third = (struct Node*) malloc(sizeof(struct Node));
 
-    // Asignar valores a cada nodo
     head->data = 1;
     head->next = second;
 
@@ -68,15 +59,12 @@ int main() {
     third->data = 3;
     third->next = head;
 
-    // Pedir al usuario que ingrese un valor para buscar
     int value;
     printf("Ingrese el valor: ");
     scanf("%d", &value);
 
-    // Buscar el valor ingresado por el usuario en la lista
     search(head, value);
 
-    // Imprimir la lista ligada circular
     printf("Mostrando estructura:\n");
     printList(head);
 
